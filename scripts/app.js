6106ea442299d4116f01ba8146767c029a4100af
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-24 17:23:39 -0500

    winner not working, found bug&&

--- scripts/app.js
+++ scripts/app.js
@@ -5,18 +5,28 @@ const app = (function (){
   function generateSquare(square){
     // if square.winner === true , 
     // give it the class ofwinner from ccsss
-
-
+    if(square.played === 0 && square.winner === true){
+      return `<div class="cell win" id="${square.position}">
+                <p> X </p>
+                </div>`;
+    }
+    if(square.played === 1 && square.winner === true){
+      return `<div class="cell win" id="${square.position}">
+                 <p> O </p>
+                  </div>`;
+    } 
     if(square.played === 0){
       return `<div class="cell" id="${square.position}">
                 <p> X </p>
                 </div>`;
     }
+   
     if(square.played === 1){
       return `<div class="cell" id="${square.position}">
                  <p> O </p>
                   </div>`;
     } 
+    
     if(square.played === null){
       return `<div class="cell" id="${square.position}">
                 <p> &nbsp; </p>
@@ -32,30 +42,61 @@ const app = (function (){
     htmlArray.splice(0,0,row);
     return htmlArray.join('');
   }
-
+  // board[0].played === board[1].played && board[1].played === board[2].played
   function checkForWinner(){
-    if( state.board[0,1,2].played === 1, or 0)
-    if (state.board[0].played === state.board[1].played === state.board[2].played && state.board[2].played !== null ){
-      
-    }
-
-
-    // [3,4,5],
-    // [6,7,8],
-    // [0,3,6],
-    // [1,4,7],
-    // [2,5,8],
-    // [0,4,8],
-    // [2,4,6],
-
+    console.log('checkforwinner ran');
+    const board =state.board;
+    if (board[0].played === board[1].played === board[2].played
+      && board[2].played !== null ){
+        console.log('winner', state.winner);
+      board[0].winner = true;
+      board[1].winner = true;
+      board[2].winner = true; }
+    if (board[3].played === board[4].played === board[5].played
+      && board[5].played !== null ){
+      board[3].winner = true;
+      board[4].winner = true;
+      board[5].winner = true; }  
+    if (board[0].played === board[3].played === board[6].played
+      && board[6].played !== null ){
+      board[0].winner = true;
+      board[3].winner = true;
+      board[6].winner = true; }
+    if (board[1].played === board[4].played === board[7].played
+        && board[7].played !== null ){
+      board[1].winner = true;
+      board[4].winner = true;
+      board[7].winner = true; }        
+    if (board[2].played === board[5].played === board[8].played
+      && board[5].played !== null ){
+      board[2].winner = true;
+      board[5].winner = true;
+      board[8].winner = true; }
+    if (board[0].played === board[4].played === board[8].played
+      && board[8].played !== null ){
+      board[0].winner = true;
+      board[4].winner = true;
+      board[8].winner = true; }   
+    if (board[2].played === board[4].played === board[6].played
+      && board[6].played !== null ){
+      board[2].winner = true;
+      board[4].winner = true;
+      board[6].winner = true; }   
+    if (board[6].played === board[7].played === board[8].played
+      && board[8].played !== null ){
+      board[6].winner = true;
+      board[7].winner = true;
+      board[8].winner = true; } 
+    else return false;  
   }
 
 
   function renderBoard() {
     console.log ('render ran');
-    if(state.winner === true){
-      // run winner state, somehow
-    } 
+   const winnerCheck = checkForWinner();
+    if(winnerCheck !==false){
+      state.winner = true;
+    }
     const board =state.board;
     const boardHtml = generateBoardString(board);
     $('.board').html(boardHtml);
